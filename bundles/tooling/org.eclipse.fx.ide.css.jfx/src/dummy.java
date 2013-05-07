31ce70b4b8847c57ee8dfcb934337d1760680ec7
Tom Schindl <tom.schindl@bestsolution.at>
2013-05-08 01:42:50 +0200

    initial checkin

---
+++ bundles/tooling/org.eclipse.fx.ide.css.jfx/src/dummy.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2011 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+
+/**
+ * @author tomschindl
+ *
+ */
+public class dummy {
+
+}
