cc2ea2474c41d2a0d82a66ca1b110059c4344188
Tom Schindl <tom.schindl@bestsolution.at>
2013-05-23 10:37:40 +0200

    NEW - bug 408403: Generate a Controller from an FXML-File  - started implementation

---
+++ bundles/tooling/org.eclipse.fx.ide.fxgraph.ui/src/org/eclipse/fx/ide/fxgraph/ui/controller/ControllerGenerator.java
@@ -0,0 +1,9 @@
+package org.eclipse.fx.ide.fxgraph.ui.controller;
+
+import org.eclipse.fx.ide.fxgraph.fXGraph.Model;
+
+public class ControllerGenerator {
+	public void generateController(Model model, boolean merge) {
+		
+	}
+}
