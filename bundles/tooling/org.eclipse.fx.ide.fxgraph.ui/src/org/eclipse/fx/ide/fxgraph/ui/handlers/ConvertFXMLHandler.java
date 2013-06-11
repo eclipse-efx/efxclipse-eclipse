d22151f26128249d1dbda6b933b4eed4e295acf7
Tom Schindl <tom.schindl@bestsolution.at>
2013-06-11 16:45:15 +0200

    NEW - bug 408390: FXML => FXGraph converter should be part of fxgraph.ui bundle

--- bundles/tooling/org.eclipse.fx.ide.fxgraph.ui/src/org/eclipse/fx/ide/fxgraph/ui/handlers/ConvertFXMLHandler.java
+++ bundles/tooling/org.eclipse.fx.ide.fxgraph.ui/src/org/eclipse/fx/ide/fxgraph/ui/handlers/ConvertFXMLHandler.java
@@ -6,27 +6,40 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *     Martin Bluehweis	<martin.bluehweis@bestsolution.at> - initial API and implementation
  *******************************************************************************/
-package org.eclipse.fx.ide.converter;
+package org.eclipse.fx.ide.fxgraph.ui.handlers;
 
 import java.io.ByteArrayInputStream;
+import java.util.Map;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.expressions.IEvaluationContext; 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.fx.ide.fxgraph.converter.FXGraphConverter;
+import org.eclipse.fx.ide.fxgraph.converter.FXMLLoader;
+import org.eclipse.fx.ide.fxgraph.fXGraph.Model;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.ToolFactory;
+import org.eclipse.jdt.core.formatter.CodeFormatter;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.text.edits.TextEdit;
 import org.eclipse.ui.ISources;
 
-public abstract class AbstractConverterHandler extends AbstractHandler {
+public class ConvertFXMLHandler extends AbstractHandler {
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IEvaluationContext context = (IEvaluationContext) event
@@ -44,6 +57,11 @@ public abstract class AbstractConverterHandler extends AbstractHandler {
 								- 1)
 						+ getTargetFileExtension()));
 				String content = convert(outFile, f);
+				IProject project = outFile.getProject();
+				Object codeFormatter = createCodeFormatter(project);
+				if (codeFormatter != null) {
+					content = format(content, (CodeFormatter) codeFormatter);
+				}
 
 				if (!outFile.exists()) {
 					try {
@@ -69,10 +87,43 @@ public abstract class AbstractConverterHandler extends AbstractHandler {
 		}
 		return null;
 	}
+	
+	protected String convert(IFile outFile, IFile file)
+			throws ExecutionException {
+		FXMLLoader loader = new FXMLLoader();
+		Model m = loader.loadModel(file);
+		return new FXGraphConverter().generate(m).toString();
+	}
 
-	protected abstract String convert(IFile outFile, IFile file)
-			throws ExecutionException;
+	protected String getTargetFileExtension() {
+		return ".fxgraph";
+	}
 
-	protected abstract String getTargetFileExtension();
+	protected String format(String contents, CodeFormatter codeFormatter) {
+		if (codeFormatter instanceof CodeFormatter) {
+			IDocument doc = new Document(contents);
+			// FIXME always returns null
+			TextEdit edit = ((CodeFormatter) codeFormatter).format(
+					CodeFormatter.K_COMPILATION_UNIT, doc.get(), 0, doc.get()
+							.length(), 0, null);
 
+			if (edit != null) {
+				try {
+					edit.apply(doc);
+					contents = doc.get();
+				} catch (Exception e) {
+					// TODO
+					e.printStackTrace();
+				}
+			}
+		}
+		return contents;
+	}
+	
+	public static Object createCodeFormatter(IProject project) {
+		IJavaProject javaProject = JavaCore.create(project);
+		@SuppressWarnings("unchecked")
+		Map<String,String> options = javaProject.getOptions(true);
+		return ToolFactory.createCodeFormatter(options);
+	}
 }
