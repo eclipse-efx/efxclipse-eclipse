b6b97e4c7fde6dcad96cb703e29d9428ebdc28b4
Tom Schindl <tom.schindl@bestsolution.at>
2013-10-22 00:37:28 +0200

    fixed NPE

--- bundles/tooling/org.eclipse.fx.ide.ui.mobile.sim.launch/src/org/eclipse/fx/ide/ui/mobile/sim/launch/MobileAppPropertyTester.java
+++ bundles/tooling/org.eclipse.fx.ide.ui.mobile.sim.launch/src/org/eclipse/fx/ide/ui/mobile/sim/launch/MobileAppPropertyTester.java
@@ -12,38 +12,43 @@ public class MobileAppPropertyTester extends PropertyTester {
 
 	@Override
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-		if( receiver instanceof IStructuredSelection ) {
-			receiver = ((IStructuredSelection)receiver).toList();
-		}
-		if( receiver instanceof List<?> ) {
-			for( Object o : (List<?>)receiver ) {
-				if( o instanceof ICompilationUnit ) {
-					ICompilationUnit u = (ICompilationUnit) o;
-					try {
-						for( IType t : u.getTypes() ) {
-							String name = t.getSuperclassName();
-							String[][] resolveType = t.resolveType(name);
-							StringBuilder b = new StringBuilder();
-							if( resolveType.length > 0 ) {
-								for( String s : resolveType[0] ) {
-									if( b.length() > 0 ) {
-										b.append(".");
-									}
-									b.append(s);
-								}	
+		try {
+			if( receiver instanceof IStructuredSelection ) {
+				receiver = ((IStructuredSelection)receiver).toList();
+			}
+			if( receiver instanceof List<?> ) {
+				for( Object o : (List<?>)receiver ) {
+					if( o instanceof ICompilationUnit ) {
+						ICompilationUnit u = (ICompilationUnit) o;
+						try {
+							for( IType t : u.getTypes() ) {
+								String name = t.getSuperclassName();
+								String[][] resolveType = t.resolveType(name);
+								StringBuilder b = new StringBuilder();
+								if( resolveType.length > 0 ) {
+									for( String s : resolveType[0] ) {
+										if( b.length() > 0 ) {
+											b.append(".");
+										}
+										b.append(s);
+									}	
+								}
+								
+								if( "org.eclipse.fx.ui.mobile.MobileApp".equals(b.toString()) ) {
+									return true;
+								}
 							}
-							
-							if( "org.eclipse.fx.ui.mobile.MobileApp".equals(b.toString()) ) {
-								return true;
-							}
+						} catch (JavaModelException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
 						}
-					} catch (JavaModelException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
 					}
 				}
 			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
+		
 		return false;
 	}
 
