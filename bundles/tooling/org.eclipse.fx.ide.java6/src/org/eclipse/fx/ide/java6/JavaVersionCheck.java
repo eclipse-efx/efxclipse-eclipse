d3ad2afa48ce5b53f4cba71b580bea6b0d27d981
Tom Schindl <tom.schindl@bestsolution.at>
2014-05-07 08:55:03 +0200

    Bug 434280 - Update Java6 check to include Java7

--- bundles/tooling/org.eclipse.fx.ide.java6/src/org/eclipse/fx/ide/java6/JavaVersionCheck.java
+++ bundles/tooling/org.eclipse.fx.ide.java6/src/org/eclipse/fx/ide/java6/JavaVersionCheck.java
@@ -10,6 +10,6 @@ public class JavaVersionCheck implements IStartup {
 	@Override
 	public void earlyStartup() {
 		StatusManager.getManager().handle(
-				new Status(IStatus.ERROR, "org.eclipse.fx.ide.java6", "You are not running your eclipse instance with Java7 or Java8. The JavaFX tooling is disabled because of this.Make Java7/8 the default system java or adjust your eclipse.ini to pass -vm pointing to your Java7/8 install.\r\n\r\nIn case you want to turn off this check open your preferences and go to General > Startup and Shutdown and uncheck 'JavaFX Tooling Java Check'"),StatusManager.SHOW);
+				new Status(IStatus.ERROR, "org.eclipse.fx.ide.java6", "You are not running your eclipse instance with Java8. The JavaFX tooling is disabled because of this.Make Java8 the default system java or adjust your eclipse.ini to pass -vm pointing to your Java8 install.\r\n\r\nIn case you want to turn off this check open your preferences and go to General > Startup and Shutdown and uncheck 'JavaFX Tooling Java Check'"),StatusManager.SHOW);
 	}
 }
