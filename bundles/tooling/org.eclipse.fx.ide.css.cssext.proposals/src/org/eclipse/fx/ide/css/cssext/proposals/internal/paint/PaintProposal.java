4cd3f9e84c793a28ee30e4ce86116e4a91a73c19
Tom Schindl <tom.schindl@bestsolution.at>
2014-08-05 09:04:04 +0200

    fixed copyright

--- bundles/tooling/org.eclipse.fx.ide.css.cssext.proposals/src/org/eclipse/fx/ide/css/cssext/proposals/internal/paint/PaintProposal.java
+++ bundles/tooling/org.eclipse.fx.ide.css.cssext.proposals/src/org/eclipse/fx/ide/css/cssext/proposals/internal/paint/PaintProposal.java
@@ -1,15 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Christoph Caks<ccaks@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.fx.ide.css.cssext.proposals.internal.paint;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
-import javafx.application.Platform;
 import javafx.embed.swt.FXCanvas;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Paint;
-import javafx.scene.transform.Scale;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 
 import org.eclipse.fx.ide.css.ui.extapi.UIProposal;
 import org.eclipse.fx.ui.controls.paint.PaintEditor;
+import org.eclipse.fx.ui.controls.paint.PaintEditor.State;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
@@ -48,22 +64,29 @@ public class PaintProposal implements UIProposal {
 	@Override
 	public boolean show() {
 		Shell shell = new Shell();
-		Platform.setImplicitExit(false);
 		FXCanvas canvas = new FXCanvas(shell, SWT.NONE);
-		PaintEditor editor = new PaintEditor(null);
+		PaintEditor editor = new PaintEditor(null, State.OK, State.CANCEL);
 		
 		Scene scene = new Scene(editor);
 		canvas.setScene(scene);
 		
 		shell.setLayout(new FillLayout());
 		shell.open();
+		shell.pack();
 		
 		AtomicBoolean bla = new AtomicBoolean();
 		bla.set(true);
 		
-		editor.setOkConsumer((o) -> {this.update(o); shell.dispose();});
-		editor.setApplyConsumer((o) -> {this.update(o);});
-		editor.setCancel(()-> {bla.set(false); shell.dispose();});
+		editor.setStateConsumer((s,o) -> {
+			if( s == State.OK ) {
+				this.update(o);
+				bla.set(true);
+				shell.dispose();
+			} else if( s == State.CANCEL ) {
+				bla.set(false);
+				shell.dispose();
+			}
+		});
 		
 		Display d = shell.getDisplay();
 		
@@ -73,17 +96,108 @@ public class PaintProposal implements UIProposal {
 			}
 		}
 		
-		
-		
-		
-		
-		return bla.get();
+		return proposal != null ? bla.get() : false;
 		
 	}
 	
 	private void update(Paint paint) {
+		if( paint instanceof LinearGradient ) {
+			LinearGradient g = (LinearGradient) paint;
+			if( g.isProportional() ) {
+				proposal = "linear-gradient( ";
+				proposal += "from "+p(g.getStartX())+"% "+p(g.getStartY())+"% ";
+				proposal += "to " + p(g.getEndX())+"% "+p(g.getEndY())+"%";
+				if( g.getCycleMethod() == CycleMethod.REFLECT ) {
+					proposal += ", reflect";
+				} else if( g.getCycleMethod() == CycleMethod.REPEAT ) {
+					proposal += ", repeat";
+				}
+				
+				proposal += stopAsString(g.getStops());
+				
+				proposal += ")";
+			} else {
+				proposal = "linear-gradient( ";
+				proposal += "from "+g.getStartX()+" "+g.getStartY()+" ";
+				proposal += "to " + g.getEndX()+" "+g.getEndY()+"";
+				if( g.getCycleMethod() == CycleMethod.REFLECT ) {
+					proposal += ", reflect";
+				} else if( g.getCycleMethod() == CycleMethod.REPEAT ) {
+					proposal += ", repeat";
+				}
+				
+				proposal += stopAsString(g.getStops());
+				
+				proposal += " )";
+			}
+		} else if( paint instanceof RadialGradient ) {
+			RadialGradient g = (RadialGradient) paint;
+			if( g.isProportional() ) {
+				proposal = "radial-gradient( ";
+				
+				proposal += "focus-angle " + g.getFocusAngle()+"deg";
+				proposal += ", focus-distance " + p(g.getFocusDistance())+"%";
+				proposal += ", center " + p(g.getCenterX())+"% " + p(g.getCenterY())+"%";
+				proposal += ", radius " + p(g.getRadius())+"%";
+				
+				if( g.getCycleMethod() == CycleMethod.REFLECT ) {
+					proposal += ", reflect";
+				} else if( g.getCycleMethod() == CycleMethod.REPEAT ) {
+					proposal += ", repeat";
+				}
+				
+				proposal += stopAsString(g.getStops());
+				proposal += " )";
+			} else {
+				proposal = "radial-gradient( ";
+				
+				proposal += "focus-angle " + g.getFocusAngle()+"deg";
+				proposal += ", focus-distance " + p(g.getFocusDistance())+"%";
+				proposal += ", center " + g.getCenterX()+ " " + g.getCenterY();
+				proposal += ", radius " + g.getRadius();
+				
+				if( g.getCycleMethod() == CycleMethod.REFLECT ) {
+					proposal += ", reflect";
+				} else if( g.getCycleMethod() == CycleMethod.REPEAT ) {
+					proposal += ", repeat";
+				}
+				
+				proposal += stopAsString(g.getStops());
+				proposal += " )";
+			}
+		} else if( paint instanceof Color ) {
+			proposal = rgb((Color) paint);
+		}
+	}
+	
+	private static String stopAsString(List<Stop> stops) {
+		return stops.stream().map((s) -> rgb(s.getColor()) + " " + p(s.getOffset())).collect(Collectors.joining(", ", ", ",""));
+	}
+	
+	private static String rgb(Color c) {
+		String rv = "";
+		if( c.getOpacity() == 1.0 ) {
+			rv += "rgb(";
+		} else {
+			rv += "rgba(";
+		}
 		
-		proposal = paint.toString();
+		rv += toInt(c.getRed())+","+toInt(c.getGreen())+","+toInt(c.getBlue());
+		
+		if( c.getOpacity() != 1.0 ) {
+			rv += "," + c.getOpacity();
+		}
+		
+		rv += ")";
+		return rv;
+	}
+	
+	private static int toInt(double v) {
+		return (int)(v * 255);
+	}
+	
+	private static double p(double v) {
+		return v * 100;
 	}
 
 	@Override
--- bundles/tooling/org.eclipse.fx.ide.css.cssext.proposals/src/org/eclipse/fx/ide/css/cssext/proposals/internal/paint/PaintProposalContributer.java
+++ bundles/tooling/org.eclipse.fx.ide.css.cssext.proposals/src/org/eclipse/fx/ide/css/cssext/proposals/internal/paint/PaintProposalContributer.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Christoph Caks<ccaks@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.fx.ide.css.cssext.proposals.internal.paint;
 
 import org.eclipse.fx.ide.css.cssext.proposal.CssExtProposalContributor;
