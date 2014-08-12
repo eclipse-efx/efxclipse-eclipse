1d2a92f9903707cb192d08e4efd317cc7397a025
Tom Schindl <tom.schindl@bestsolution.at>
2014-08-12 23:40:27 +0200

    Bug 441453 - Compare editor fails on CSS-Files

---
+++ bundles/tooling/org.eclipse.fx.ide.css.ui/src/org/eclipse/fx/ide/css/ui/editor/WTPCompatibleDocumentProvider.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2011 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.ide.css.ui.editor;
+
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
+
+/**
+ * Document provider for compare editor
+ */
+public class WTPCompatibleDocumentProvider extends TextFileDocumentProvider /*org.eclipse.xtext.ui.editor.model.XtextDocumentProvider*/ {
+	//nothing for now
+}
