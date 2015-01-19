8e9c47c36deb6327a509cce1a80582c3d95df86e
Tom Schindl <tom.schindl@bestsolution.at>
2015-01-19 13:37:20 +0100

    Bug 457825 - E(fx)clipse: onAction event handles is marked as error in FXML if not marked as @FXML

--- bundles/tooling/org.eclipse.fx.ide.model/src/org/eclipse/fx/ide/model/internal/FXCtrlClass.java
+++ bundles/tooling/org.eclipse.fx.ide.model/src/org/eclipse/fx/ide/model/internal/FXCtrlClass.java
@@ -21,6 +21,7 @@ import org.eclipse.fx.ide.model.IFXCtrlClass;
 import org.eclipse.fx.ide.model.IFXCtrlEventMethod;
 import org.eclipse.fx.ide.model.IFXCtrlField;
 import org.eclipse.fx.ide.model.internal.utils.Util;
+import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.IAnnotation;
 import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IJavaProject;
@@ -37,14 +38,14 @@ public class FXCtrlClass implements IFXCtrlClass {
 	private Map<String,IFXCtrlEventMethod> eventMethods;
 	private Map<String,IFXCtrlField> fields;
 	private IFXCtrlClass superClass;
-	
+
 	public FXCtrlClass(IJavaProject jp, IType type) {
 		this.javaProject = jp;
 		this.type = type;
-		
+
 		try {
 			String s = type.getSuperclassName();
-			
+
 			if( s != null ) {
 				s = getFQNType(type,s);
 				superClass = FXPlugin.getClassmodel().findCtrlClass(jp, jp.findType(s));
@@ -53,12 +54,12 @@ public class FXCtrlClass implements IFXCtrlClass {
 			FXPlugin.getLogger().log(LogService.LOG_ERROR, "Unable to retrieve superclass name of '"+type.getFullyQualifiedName()+"'", e );
 		}
 	}
-	
+
 	@Override
 	public IType getType() {
 		return type;
 	}
-	
+
 	IJavaProject getJavaProject() {
 		return javaProject;
 	}
@@ -71,7 +72,7 @@ public class FXCtrlClass implements IFXCtrlClass {
 	public String getFQN() {
 		return type.getFullyQualifiedName();
 	}
-	
+
 	@Override
 	public Map<String,IFXCtrlEventMethod> getAllEventMethods() {
 		Map<String, IFXCtrlEventMethod> rv = new HashMap<String, IFXCtrlEventMethod>();
@@ -83,45 +84,47 @@ public class FXCtrlClass implements IFXCtrlClass {
 	}
 
 	private Map<String,IFXCtrlEventMethod> getLocalEventMethods() {
-		if( eventMethods == null )  {
+		if( this.eventMethods == null )  {
 			eventMethods = new HashMap<String, IFXCtrlEventMethod>();
 			try {
 				for( IMethod m : type.getMethods() ) {
-					boolean annotated = false;
-					for (IAnnotation a : m.getAnnotations()) {
-						if (a.getElementName().endsWith("FXML")) {
-							annotated = true;
-							break;
+					boolean annotated = Flags.isPublic(m.getFlags());
+					if( ! annotated ) {
+						for (IAnnotation a : m.getAnnotations()) {
+							if (a.getElementName().endsWith("FXML")) {
+								annotated = true;
+								break;
+							}
 						}
 					}
-					
+
 					if( annotated ) {
 						String[] types = m.getParameterTypes();
 						if( types.length <= 1 ) {
 							if( types.length == 1 )  {
 								String erasedFQNType = Util.getFQNType((IType)m.getParent(), Signature.getTypeErasure(Signature.toString(types[0])));
 								if( FXCtrlEventMethod.isEventMethod(javaProject, erasedFQNType) ) {
-									eventMethods.put(m.getElementName(), new FXCtrlEventMethod(this,m, erasedFQNType));	
+									eventMethods.put(m.getElementName(), new FXCtrlEventMethod(this,m, erasedFQNType));
 								}
 							} else {
 								// Only if there's not already a method with the same id
 								if( ! eventMethods.containsKey(m.getElementName()) ) {
-									eventMethods.put(m.getElementName(), new FXCtrlEventMethod(this,m,null));	
+									eventMethods.put(m.getElementName(), new FXCtrlEventMethod(this,m,null));
 								}
 							}
 						}
 					}
-					
+
 				}
 			} catch (JavaModelException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 		return eventMethods;
 	}
-	
+
 	@Override
 	public Map<String,IFXCtrlField> getAllFields() {
 		Map<String, IFXCtrlField> rv = new HashMap<String, IFXCtrlField>();
@@ -131,20 +134,22 @@ public class FXCtrlClass implements IFXCtrlClass {
 		rv.putAll(getLocalFields());
 		return Collections.unmodifiableMap(rv);
 	}
-	
+
 	private Map<String,IFXCtrlField> getLocalFields() {
 		if( fields == null ) {
 			fields = new HashMap<String, IFXCtrlField>();
 			try {
 				for( IField f : type.getFields() ) {
-					boolean annotated = false;
-					for (IAnnotation a : f.getAnnotations()) {
-						if (a.getElementName().endsWith("FXML")) {
-							annotated = true;
-							break;
+					boolean annotated = Flags.isPublic(f.getFlags());
+					if( ! annotated ) {
+						for (IAnnotation a : f.getAnnotations()) {
+							if (a.getElementName().endsWith("FXML")) {
+								annotated = true;
+								break;
+							}
 						}
 					}
-					
+
 					if( annotated ) {
 						String erasedFQNType = Util.getFQNType((IType)f.getParent(), Signature.getTypeErasure(Signature.toString(f.getTypeSignature())));
 						FXCtrlField field = new FXCtrlField(this, f, erasedFQNType);
@@ -155,9 +160,9 @@ public class FXCtrlClass implements IFXCtrlClass {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
 		return fields;
 	}
 }
