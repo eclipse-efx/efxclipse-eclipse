daf000fc87c0099e306eb1a6bbf0882d7c6142e1
Tom Schindl <tom.schindl@bestsolution.at>
2015-06-09 11:35:27 +0200

    fixed compiler errors

--- bundles/tooling/org.eclipse.fx.ide.css.ui/src/org/eclipse/fx/ide/css/ui/internal/PreviewAdapterFactory.java
+++ bundles/tooling/org.eclipse.fx.ide.css.ui/src/org/eclipse/fx/ide/css/ui/internal/PreviewAdapterFactory.java
@@ -15,17 +15,22 @@ import org.eclipse.fx.ide.ui.editor.IFXPreviewAdapter;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 
-
+@SuppressWarnings("restriction")
 public class PreviewAdapterFactory implements IAdapterFactory {
 
 	@Override
-	@SuppressWarnings("rawtypes")
-	public Object getAdapter(Object adaptableObject, Class adapterType) {
-		if(adapterType == IFXPreviewAdapter.class && adaptableObject instanceof XtextEditor ) {
+	public Class<?>[] getAdapterList() {
+		return new Class[] { IFXPreviewAdapter.class };
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T getAdapter(Object adaptableObject, Class<T> adapterType) {
+		if (adapterType == IFXPreviewAdapter.class && adaptableObject instanceof XtextEditor) {
 			final XtextEditor editor = (XtextEditor) adaptableObject;
-			if( "org.eclipse.fx.ide.css.CssDsl".equals(editor.getLanguageName()) ) {
-				return new IFXPreviewAdapter() {
-					
+			if ("org.eclipse.fx.ide.css.CssDsl".equals(editor.getLanguageName())) { //$NON-NLS-1$
+				return (T) new IFXPreviewAdapter() {
+
 					@Override
 					public IEditorPart getEditorPart() {
 						return editor;
@@ -33,13 +38,7 @@ public class PreviewAdapterFactory implements IAdapterFactory {
 				};
 			}
 		}
-		return null;
-	}
-
-	@Override
-	@SuppressWarnings("rawtypes")
-	public Class[] getAdapterList() {
-		return new Class[]{IFXPreviewAdapter.class};
+		return (T) null;
 	}
 
 }
