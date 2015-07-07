d536b0e02421bec0d8a85a874a4fd07035f5bc69
Tom Schindl <tom.schindl@bestsolution.at>
2015-07-07 10:17:25 +0200

    Bug 472022 - NullPointerException in FXVersionUtil.getFxVersion (11)

--- bundles/tooling/org.eclipse.fx.ide.jdt.core/src/org/eclipse/fx/ide/jdt/core/FXVersionUtil.java
+++ bundles/tooling/org.eclipse.fx.ide.jdt.core/src/org/eclipse/fx/ide/jdt/core/FXVersionUtil.java
@@ -3,14 +3,27 @@ package org.eclipse.fx.ide.jdt.core;
 import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.IVMInstall2;
 
+/**
+ * Check the java version
+ */
 public class FXVersionUtil {
-
+	/**
+	 * Check the version for the given java install
+	 * 
+	 * @param i
+	 *            the installation
+	 * @return the value
+	 */
 	public static FXVersion getFxVersion(IVMInstall i) {
 		if (i instanceof IVMInstall2) {
 			final String javaVersion = ((IVMInstall2) i).getJavaVersion();
-			if (javaVersion.startsWith("1.8")) {
+			if (javaVersion == null) {
+				return FXVersion.UNKNOWN;
+			}
+
+			if (javaVersion.startsWith("1.8")) { //$NON-NLS-1$
 				return FXVersion.FX8;
-			} else if (javaVersion.startsWith("1.7")) {
+			} else if (javaVersion.startsWith("1.7")) { //$NON-NLS-1$
 				return FXVersion.FX2;
 			}
 		}
