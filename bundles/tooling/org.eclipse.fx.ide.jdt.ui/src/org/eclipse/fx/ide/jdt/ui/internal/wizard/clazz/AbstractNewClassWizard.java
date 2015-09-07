05b73d87c3528e5cdec727bc5e85feedf120bd1e
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-07 10:55:21 +0200

    Bug 476765 - ClassCastException in AbstractNewClassWizard.createFile (35)

--- bundles/tooling/org.eclipse.fx.ide.jdt.ui/src/org/eclipse/fx/ide/jdt/ui/internal/wizard/clazz/AbstractNewClassWizard.java
+++ bundles/tooling/org.eclipse.fx.ide.jdt.ui/src/org/eclipse/fx/ide/jdt/ui/internal/wizard/clazz/AbstractNewClassWizard.java
@@ -12,6 +12,7 @@ package org.eclipse.fx.ide.jdt.ui.internal.wizard.clazz;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.fx.ide.ui.wizards.AbstractNewJDTElementWizard;
 import org.eclipse.fx.ide.ui.wizards.template.JDTElement;
@@ -21,20 +22,26 @@ import org.eclipse.ui.INewWizard;
 
 
 public abstract class AbstractNewClassWizard<O extends JDTElement> extends AbstractNewJDTElementWizard<O> implements INewWizard {
-	
+
 	@Override
 	protected IFile createFile() {
 		IPackageFragment fragment = getDomainClass().getPackageFragment();
 		if (fragment != null) {
 			String cuName = getDomainClass().getName() + ".java";
-			ICompilationUnit unit = fragment.getCompilationUnit(cuName); 
+			ICompilationUnit unit = fragment.getCompilationUnit(cuName);
 			IResource resource = unit.getResource();
 			return (IFile) resource;
 		} else {
 			String cuName = getDomainClass().getName() + ".java";
-			IFolder p = (IFolder) getDomainClass().getFragmentRoot().getResource();
-			return p.getFile(cuName);
+			if( getDomainClass().getFragmentRoot().getResource() instanceof IProject ) {
+				IProject p = (IProject) getDomainClass().getFragmentRoot().getResource();
+				return p.getFile(cuName);
+			} else {
+				IFolder p = (IFolder) getDomainClass().getFragmentRoot().getResource();
+				return p.getFile(cuName);
+			}
+
 		}
 	}
-	
+
 }
