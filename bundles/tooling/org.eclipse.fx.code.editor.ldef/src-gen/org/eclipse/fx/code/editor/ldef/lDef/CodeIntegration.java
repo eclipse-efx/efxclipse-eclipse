3b646be867cef7a71691982ea662f9683fe5c8fd
Tom Schindl <tom.schindl@bestsolution.at>
2017-05-18 12:33:03 +0200

    refs #5

---
+++ bundles/tooling/org.eclipse.fx.code.editor.ldef/src-gen/org/eclipse/fx/code/editor/ldef/lDef/CodeIntegration.java
@@ -0,0 +1,19 @@
+/**
+ */
+package org.eclipse.fx.code.editor.ldef.lDef;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Code Integration</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.eclipse.fx.code.editor.ldef.lDef.LDefPackage#getCodeIntegration()
+ * @model
+ * @generated
+ */
+public interface CodeIntegration extends EObject
+{
+} // CodeIntegration
