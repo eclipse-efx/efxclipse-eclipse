7604759678cc95778a4754022d902e3730f6661c
Martin Bluehweis <martin.bluehweis@bestsolution.at>
2019-08-23 16:11:50 +0200

    fixes #66 Add arguments tab to Maven-OSGI-Launchconfigurations to support Program/VM arguments

--- bundles/tooling/org.eclipse.fx.ide.mvnosgi.launching.ui/src/org/eclipse/fx/ide/mvnosgi/launching/ui/OSGiLaunchConfigurationTabGroup.java
+++ bundles/tooling/org.eclipse.fx.ide.mvnosgi.launching.ui/src/org/eclipse/fx/ide/mvnosgi/launching/ui/OSGiLaunchConfigurationTabGroup.java
@@ -3,16 +3,13 @@ package org.eclipse.fx.ide.mvnosgi.launching.ui;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
 
 public class OSGiLaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
 
-	public OSGiLaunchConfigurationTabGroup() {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
-		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] { new MainLauncherTab() };
+		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] { new MainLauncherTab(), new JavaArgumentsTab() };
 		setTabs(tabs);
 	}
 
